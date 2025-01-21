412326dab326ad458829821fca9c4dabbd1d2959
Cherrie Bergdoll <3292658709@qq.com>
2025-01-21 09:35:35 +0800

    Lo研社

---
+++ app/nuxt.config.ts
@@ -0,0 +1,5 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  extends: '../',
+  compatibilityDate: '2025-01-21',
+})
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { awesome } = useAppConfig()
+definePageMeta({ layout: 'page' })
+useHead({ titleTemplate: '', title: awesome?.name || 'Lo研社' })
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen w-screen">
+    <div class="text-4xl font-bold">Lo研社</div>
+  </div>
+</template>
