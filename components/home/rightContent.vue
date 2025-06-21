555424cb978f1fe1a578a48104c7d5af50afb6e4
Cherrie Bergdoll <3292658709@qq.com>
2025-06-21 11:11:05 +0800

    增加3D场景
    修改部分配置项 添加公共方法

---
+++ components/home/rightContent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    右侧内容
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+<style>
+
+</style>
---
+++ utils/ipConfig.ts
@@ -0,0 +1,2 @@
+// export const BASE_IMG: string = 'http://localhost:3001/ali/'
+export const BASE_IMG: string = 'https://lolitalibrary.com/ali/'
